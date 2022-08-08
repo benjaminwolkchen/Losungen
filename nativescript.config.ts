339eaaa20d41cb8c7ac76501ae5c1b3302e9478a
Benjamin Hartmann <hbenjamin@tutanota.de>
2022-08-08 13:46:52 +0200

    Tweak some settings

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.LosungenNative',
+  id: 'de.awesomebible.Losungen',
   appPath: 'app',
   appResourcesPath: 'App_Resources',
   android: {
